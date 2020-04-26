43fed25a79db34df9caf1f21db8d627ddf0025c8
Doğancan <53553597+daksak7@users.noreply.github.com>
2020-04-26 22:47:29 +0300

    main template added
    bootstrap added.
    navbar added.
    Page template split 3 parts.

---
+++ header.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" type="text/css" href="/css/styles.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+
+    <title>Jiten TR</title>
+  </head>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="index.php">JitenTR</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="ara.php">Ara <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="defter.php">Defter</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">JLPT</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Giriş</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Kayıt Ol</a>
+      </li>
+    </ul>
+  </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include 'header.php'?>
+  <body>
+    <h1>Hello, World!</h1>
+</body>
+<?php include 'footer.php'?>
